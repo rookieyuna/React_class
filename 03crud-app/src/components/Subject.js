bb5d4f2f54d27ff6ddd96c82858f941aa9933758
syjak7 <syjak7@gmail.com>
2022-01-12 00:27:00 +0900

    22.01.11 수업커밋

---
+++ 03crud-app/src/components/Subject.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+/*
+CDN방식에서는 React.Component를 상속했지만, 
+웹펙(web-pack)방식에서는 아래와 같이 상속하면 된다.
+컴포넌트를 만들때에는 항상 하나의 최상위 태그만 있어야한다
+*/
+class Subject extends Component{
+    render(){
+        return(
+        <header>
+            <h1>{this.props.title}</h1>
+            {this.props.sub}
+            </header>
+        );
+    }
+}
+
+export default Subject;
